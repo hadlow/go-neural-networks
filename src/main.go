76a16f542e15411c831d05c5d9e6cd3516d869fc
Billy Hadlow <me@billyhadlow.com>
2021-04-18 14:17:51 +0100

    rand martrix

--- src/main.go
+++ src/main.go
@@ -1,16 +1,35 @@
 package main
 
 import "fmt"
+import "math/rand"
 import "gonum.org/v1/gonum/mat"
 
-func pMatrix(name string, matrix *mat.Dense) {
+func fmtMatrix(name string, matrix *mat.Dense) {
 	fMatrix := mat.Formatted(matrix, mat.Prefix("    "), mat.Squeeze())
 
 	fmt.Printf(name + " = %v", fMatrix)
 }
 
-func main() {
-	zero := mat.NewDense(3, 5, nil)
+func randMatrix(x int, y int) *mat.Dense {
+	data := make([]float64, x * y)
 
-	pMatrix("b", zero)
+	for i := range data {
+		data[i] = rand.NormFloat64()
+	}
+
+	return mat.NewDense(x, y, data)
+}
+
+// Hyperparameters
+var n_inputs int = 5
+var n_hidden int = 3
+var n_output int = 2
+
+func main() {
+	b := mat.NewDense(3, 5, nil)
+	w := randMatrix(6, 4)
+
+	fmt.Println(w)
+	fmtMatrix("b", b)
+	//fmtMatrix("w", w)
 }
